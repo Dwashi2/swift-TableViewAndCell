a7e45ed80323c2d8ce80bd1e91881ed973a6c1b8
Daniel Washington <danielvertigo@hotmail.com>
2021-07-07 20:53:35 -0300

    Initial Commit

---
+++ TableViewXibCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewXibCell
+//
+//  Created by Daniel Washington Ignacio on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
